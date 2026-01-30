e38c60cb8a8b473ce521d9833c3132abb1cef73e
hunterhw <257879516+hunterhw@users.noreply.github.com>
2026-01-30 04:28:46 +0200

    Update api.js

--- webapp/lib/api.js
+++ webapp/lib/api.js
@@ -1,23 +1,23 @@
-import { getInitData } from "./tg";
+import { waitForInitData } from "./tg";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
+async function initHeader() {
+  const initData = await waitForInitData();
+  return { "x-telegram-initdata": initData };
+}
+
 export async function apiGet(path) {
-  const initData = getInitData();
-  const res = await fetch(`${API_BASE}${path}`, {
-    headers: { "x-telegram-initdata": initData }
-  });
+  const headers = await initHeader();
+  const res = await fetch(`${API_BASE}${path}`, { headers });
   return res.json();
 }
 
 export async function apiPost(path, body) {
-  const initData = getInitData();
+  const headers = await initHeader();
   const res = await fetch(`${API_BASE}${path}`, {
     method: "POST",
-    headers: {
-      "content-type": "application/json",
-      "x-telegram-initdata": initData
-    },
+    headers: { ...headers, "content-type": "application/json" },
     body: JSON.stringify(body)
   });
   return res.json();
